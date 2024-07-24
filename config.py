838336f0eaf368552dde4c20b125271b7be3f907
taewoo-dev <twin9774@gmail.com>
2024-07-24 20:11:40 +0900

    블로그 크롤링

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import pymysql.cursors
 CONNECTION = pymysql.connect(
     host="localhost",
     user="root",
-    password="1520528a",
+    password="--------",
     database="diningcode",
     charset="utf8mb4",
     cursorclass=pymysql.cursors.DictCursor  # json 형식으로 데이터가 전달되는 경우가 많다
